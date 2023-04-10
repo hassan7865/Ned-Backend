d0dacf57c7ba35e32421e95be04436f2e3369259
Hassan Siddiqui <103386392+hassan7865@users.noreply.github.com>
2023-04-11 04:20:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://nedadmin.netlify.app",
     methods:["GET","POST","PUT","DELETE"]
 }))
 const connect = ()=>{
